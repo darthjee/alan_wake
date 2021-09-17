e75b88f65e4af1ab7814e11c2ca1ee0760cc05f8
darthjee <darthjee@gmail.com>
2021-09-17 18:02:29 +0200

    Add users table

---
+++ source/db/migrate/20210917152359_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :login, null: false, unique: true
+      t.string :email, null: false, unique: true
+      t.string :encrypted_password, null: false
+      t.string :salt, null: false
+      t.string :name, null: false
+      t.timestamps
+    end
+
+    create_table :sessions do |t|
+      t.bigint :user_id, null: false
+      t.datetime :expiration
+      t.timestamp :created_at
+      t.foreign_key :users, column: :user_id
+    end
+  end
+end
--- source/db/schema.rb
+++ source/db/schema.rb
@@ -10,6 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_09_17_152359) do
 
+  create_table "sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "expiration"
+    t.timestamp "created_at"
+    t.index ["user_id"], name: "fk_rails_758836b4f0"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "login", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "salt", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "sessions", "users"
 end
